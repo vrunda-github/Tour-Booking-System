611924d8216248214f5792400986152b18231840
vrunda-github <vrundajobanputra09@gmail.com>
2022-06-29 15:07:09 +0100

    Initial Commit
    First Commit

---
+++ src/main/java/com/tbs/com/java/TourBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tbs.com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbs/com/java/TourBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbs.com.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
